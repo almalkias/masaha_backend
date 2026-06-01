463ff9893f980e7753e5c490a3c9e14c6aa31b6e
almalkias <almalkias@icloud.com>
2026-06-01 20:28:02 +0300

    update settings.py

--- store/settings.py
+++ store/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://iqtinaa-backend.onrender.com",
+    "https://iqtinaaart.netlify.app",
 ]
 
 # Application definition
